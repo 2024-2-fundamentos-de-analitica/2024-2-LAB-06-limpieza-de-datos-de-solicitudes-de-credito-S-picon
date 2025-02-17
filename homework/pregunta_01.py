87f0265999ce089f6909974a915a7e35fe484a33
S-picon <spicon@unal.edu.co>
2025-02-17 14:35:49 -0500

    Tarea Completa Limpieza de DT Sol credits

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,6 +2,23 @@
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
 
+import os
+import pandas as pd
+
+
+def clean_text(columna):
+    return columna.str.lower().str.replace("-", " ").str.replace("_", " ").str.strip()
+
+
+
+def dates(datos_series):
+    datos = pd.to_datetime(datos_series, dayfirst=True, errors="coerce")
+    datos = datos.fillna(
+        pd.to_datetime(datos_series, format="%Y/%m/%d", errors="coerce")
+    )
+    return datos
+
+
 
 def pregunta_01():
     """
@@ -13,3 +30,44 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    carpeta_entrada = "files/input/solicitudes_de_credito.csv"
+    carpeta_salida = "files/output/solicitudes_de_credito.csv"
+
+    df = pd.read_csv(carpeta_entrada, sep=";", index_col=0)
+
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+    df["barrio"] = df["barrio"].str.lower().str.replace("_", " ").str.replace("-", " ")
+    columns = [
+        "idea_negocio",
+        "línea_credito",
+    ]
+    for column in columns:
+        df[column] = clean_text(df[column])
+
+    df["monto_del_credito"] = (
+        df["monto_del_credito"]
+        .str.strip()
+        .str.replace("$", "")
+        .str.replace(",", "")
+        .str.replace(".00", "")
+        .astype(int)
+    )
+    df["fecha_de_beneficio"] = dates(df["fecha_de_beneficio"])
+
+    df.drop_duplicates(inplace=True)
+
+    if os.path.exists(carpeta_salida):
+        os.remove(carpeta_salida)
+
+    os.makedirs(os.path.dirname(carpeta_salida), exist_ok=True)
+    df.to_csv(carpeta_salida, sep=";")
+
+    print(df["sexo"].value_counts())
+    print(df["barrio"].value_counts())
+
+
+pregunta_01()
